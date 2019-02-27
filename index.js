2753c77f375f5b74a91c4e7740891078f0461e4e
Ignacio Olvera <ignacio.olvera766@yahoo.com.mx>
2019-02-26 22:32:16 -0600

    Producción v 1.0

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 4000;
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/reportes', { useNewUrlParser: true }, (err, res) => {
+
+mongoose.connect('mongodb://heroku_5dv87bb4:heroku_5dv87bb4@ds163683.mlab.com:63683/heroku_5dv87bb4', { useNewUrlParser: true }, (err, res) => {
     if (err) {
         throw err;
     } else {
@@ -22,3 +23,13 @@ mongoose.connect('mongodb://localhost:27017/reportes', { useNewUrlParser: true }
         });
     }
 });
+
+// mongoose.connect('mongodb://localhost:27017/reportes', { useNewUrlParser: true }, (err, res) => {
+//     if (err) {
+//         throw err;
+//     } else {
+//         app.listen(port, function () {
+//             console.log("La base de datos está corriendo correctamente");
+//         });
+//     }
+// });
